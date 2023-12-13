27e9cc1f8cfb9313a3d3e22e4fd0a1ec2a22e410
Mar <mar@strawmelonjuice.com>
2023-12-13 01:34:21 +0100

    Adapt for pluginloader v2

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const path = require("path");
 const hlimg_config = require(path.join(__dirname, "/config.json"));
-const express = require("express");
-module.exports = {
-	CyntiaPluginCompat: 1,
-	modifyBodyHTML (htmlin) {
-		return `${htmlin}<script id="hlimg-options" type="application/json">
-        ${JSON.stringify(
-			hlimg_config
-		)}</script>
-<script defer type="module" src="/hl-img/hl-img.min.js"></script>`;
-	},
-    expressActions(expressapp) {
-        expressapp.use(
-					"/hl-img",
-					express.static(path.join(__dirname, "/node_modules/hl-img/dist/"))
-				);
-    }
-};
+switch (process.argv[2]) {
+  case "body":
+    console.log(`${
+      process.argv[3]
+    }<script id="hlimg-options" type="application/json">
+        ${JSON.stringify(hlimg_config)}</script>
+<script defer type="module" src="/e/hl-img/hl-img.min.js"></script>`);
+return;
+}
