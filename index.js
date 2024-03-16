af613e75a0b9614f4bfa49c0336b70c5bc6e7491
Mar <mar@strawmelonjuice.com>
2024-03-16 12:49:50 +0100

    Put it in head

--- index.js
+++ index.js
@@ -28,9 +28,6 @@ if (fs.existsSync(configfile)) {
 } else {
 	fs.writeFileSync(configfile, JSON.stringify(config));
 }
-switch (process.argv[2]) {
-	case "body":
-		console.log(`${process.argv[3]}<script id="hlimg-options" type="application/json">
+console.log(`${process.argv[2]}<script id="hlimg-options" type="application/json">
         ${JSON.stringify(config)}</script>
 <script defer type="module" src="/e/hl-img/hl-img.min.js"></script>`);
-}
